23c3f0b0a1bd00984f0a0aedae92349ba335db6d
Tom Bentley <tom.bentley@cantab.net>
2015-07-22 10:40:25 +0100

    Changes for the type of meta literals.

--- src/com/redhat/ceylon/compiler/typechecker/analyzer/ExpressionVisitor.java
+++ src/com/redhat/ceylon/compiler/typechecker/analyzer/ExpressionVisitor.java
@@ -8747,7 +8747,11 @@ public class ExpressionVisitor extends Visitor {
                                 getDeclarationReferenceSuggestion(d));
                     }
                 }
-                that.setTypeModel(unit.getConstructorDeclarationType());
+                if (((Constructor)d).getContainer().getMember(d.getName(), null, false) instanceof Value) {
+                    that.setTypeModel(unit.getValueConstructorDeclarationType());
+                } else {
+                    that.setTypeModel(unit.getCallableConstructorDeclarationType());
+                }
             }
             else if (that instanceof Tree.InterfaceLiteral) {
                 if (!(d instanceof Interface)) {
@@ -8999,7 +9003,10 @@ public class ExpressionVisitor extends Visitor {
                         value.appliedTypedReference(outerType, 
                                 NO_TYPE_ARGS);
                 that.setTarget(reference);
-                that.setTypeModel(unit.getValueMetatype(reference));
+                Type metatype = constructor ?
+                        unit.getValueConstructorMetatype(reference):
+                        unit.getValueMetatype(reference);
+                that.setTypeModel(metatype);
             }
         }
     }
